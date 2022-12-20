9e210a516162060421337fe7cbe22bee717dad12
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-12-20 13:55:24 -0300

    Inserindo template HTML para o componente de cadastro

--- gerenciador-tarefas/src/app/tarefas/cadastrar/cadastrar-tarefas.component.ts
+++ gerenciador-tarefas/src/app/tarefas/cadastrar/cadastrar-tarefas.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar-tarefas',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class CadastrarTarefasComponent {
 
+  @ViewChild('formTarefa', { static: true }) 
+  formTarefa: NgForm;
+  
 }
