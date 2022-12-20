c25582f2d05fb5a46bd10021b657e23d9340b854
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-12-20 12:30:56 -0300

    Configurando rotas de navegação da aplicação

--- gerenciador-tarefas/src/app/app-routing.module.ts
+++ gerenciador-tarefas/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TarefaRoutes } from './tarefas';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/tarefas/listar',
+    pathMatch: 'full'
+  },
+  ...TarefaRoutes //concatenando as rotas do Tarefas routes, as rotas principais
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
