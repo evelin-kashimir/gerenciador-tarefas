ee41a957825126af217522739b3fa8a66ac46547
Evelin Ferraz <evelin.moitinho@capgemini.com>
2022-12-20 12:10:04 -0300

    Criando o componente para listar as tarefas

--- gerenciador-tarefas/src/app/tarefas/tarefas.module.ts
+++ gerenciador-tarefas/src/app/tarefas/tarefas.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TarefaService } from './shared';
+import { ListarTarefaComponent } from './listar';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarTarefaComponent
+  ],
   //modulos
   imports: [
     CommonModule
